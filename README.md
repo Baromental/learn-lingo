3701487caa173851836b0d15dded819bc0ea2d58
Maksym <150538968+Baromental@users.noreply.github.com>
2025-05-27 19:00:11 +0300

    preparation

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-react-app-template Репозиторій для зручного створення нового проекту на основі
-React
+Learn Lingo
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Favorites } from 'pages/Favorites/Favorites';
+import { Home } from 'pages/Home/Home';
+import { Teachers } from 'pages/Teachers/Teachers';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Teachers />
+      <Favorites />
+    </div>
+  );
+};
+
+export default App;
