93815c5ab3313d473443f6e9763beb9e718aa543
Maksym <150538968+Baromental@users.noreply.github.com>
2025-05-28 20:42:34 +0300

    markup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>
+            Unlock your potential with the best<span>language</span>tutors
+          </h1>
+          <p>
+            Embark on an Exciting Language Journey with Expert Language Tutors:
+            Elevate your language proficiency to new heights by connecting with
+            highly qualified and experienced tutors.
+          </p>
+          <button>Get started</button>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <p>32,000 +</p>
+            <p>Experienced tutors</p>
+          </li>
+          <li>
+            <p>300,000 +</p>
+            <p>5-star tutor reviews</p>
+          </li>
+          <li>
+            <p>120 +</p>
+            <p>Subjects taught</p>
+          </li>
+          <li>
+            <p>200 +</p>
+            <p>Tutor nationalities</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
