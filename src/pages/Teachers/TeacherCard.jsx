ffafc9236a6d8e22e52a8fd3c94ff778fc2c3eb4
Maksym <150538968+Baromental@users.noreply.github.com>
2025-05-31 19:42:36 +0300

    markup for teachers card

--- src/pages/Teachers/TeacherCard.jsx
+++ src/pages/Teachers/TeacherCard.jsx
@@ -1,5 +1,63 @@
 import React from 'react';
 
 export const TeacherCard = () => {
-  return <div>TeacherCard</div>;
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>
+          <div>
+            <p>Languages</p>
+            <p>Name</p>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <i></i>
+                Lessons online
+              </li>
+              <li>Lessons done: </li>
+              <li>
+                <i></i>
+                Rating:
+              </li>
+              <li>
+                Price / 1 hour: <span></span>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button></button>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>Speaks:</li>
+            <li>Lesson Info:</li>
+            <li>Conditions:</li>
+          </ul>
+        </div>
+        <p></p>
+        <ul>
+          <li>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <p></p>
+                <p>
+                  <i></i>
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <ul>
+          <button></button>
+        </ul>
+        <button></button>
+      </div>
+    </div>
+  );
 };
