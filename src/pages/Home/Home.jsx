377fda0ace9402be298dd9d01cab24058f92d763
Maksym <150538968+Baromental@users.noreply.github.com>
2025-06-05 14:43:44 +0300

    home css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,41 +1,42 @@
 import React from 'react';
+import s from './Home.modules.css';
 
 export const Home = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h1>
+    <div className={s.container}>
+      <div className={s.parImgContainer}>
+        <div className={s.parContainer}>
+          <h1 className={s.header}>
             Unlock your potential with the best<span>language</span>tutors
           </h1>
-          <p>
+          <p className={s.paragraph}>
             Embark on an Exciting Language Journey with Expert Language Tutors:
             Elevate your language proficiency to new heights by connecting with
             highly qualified and experienced tutors.
           </p>
-          <button>Get started</button>
+          <button className={s.button}>Get started</button>
         </div>
         <div>
           <img src="" alt="" />
         </div>
       </div>
-      <div>
+      <div className={s.listContainer}>
         <ul>
           <li>
-            <p>32,000 +</p>
-            <p>Experienced tutors</p>
+            <p className={s.digit}>32,000 +</p>
+            <p className={s.text}>Experienced tutors</p>
           </li>
           <li>
-            <p>300,000 +</p>
-            <p>5-star tutor reviews</p>
+            <p className={s.digit}>300,000 +</p>
+            <p className={s.text}>5-star tutor reviews</p>
           </li>
           <li>
-            <p>120 +</p>
-            <p>Subjects taught</p>
+            <p className={s.digit}>120 +</p>
+            <p className={s.text}>Subjects taught</p>
           </li>
           <li>
-            <p>200 +</p>
-            <p>Tutor nationalities</p>
+            <p className={s.digit}>200 +</p>
+            <p className={s.text}>Tutor nationalities</p>
           </li>
         </ul>
       </div>
