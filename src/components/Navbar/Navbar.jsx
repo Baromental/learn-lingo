45192124fe81c43c6fa0f8d634b74a167139c85e
Maksym <150538968+Baromental@users.noreply.github.com>
2025-06-05 18:38:05 +0300

    navbar icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router';
+import flag from '../../images/flag.svg';
+import login from '../../images/login.svg';
+
 import s from './Navbar.module.css';
 
 export const Navbar = () => {
   return (
     <div className={s.container}>
       <NavLink className={s.logo} to="/">
-        <span></span>LearnLingo
+        <img src={flag} alt="flag"></img>LearnLingo
       </NavLink>
       <div className={s.linksContainer}>
         <NavLink className={s.link} to="/">
@@ -18,7 +21,7 @@ export const Navbar = () => {
       </div>
       <div className={s.buttonsContainer}>
         <button className={s.logButton}>
-          <span></span>Log in
+          <img className={s.icon} src={login} alt="login"></img>Log in
         </button>
         <button className={s.regButton}>Registration</button>
       </div>
