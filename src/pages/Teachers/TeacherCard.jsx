a69da9378918bfb8480ab46967f2c665621be6c0
Maksym <150538968+Baromental@users.noreply.github.com>
2025-06-17 21:44:37 +0300

    Read more logic

--- src/pages/Teachers/TeacherCard.jsx
+++ src/pages/Teachers/TeacherCard.jsx
@@ -1,106 +1,13 @@
-import React, { useState } from 'react';
-import star from '../../images/star.svg';
-import heart from '../../images/heart.svg';
-import book from '../../images/book.svg';
+import React from 'react';
+import { TeacherCardItem } from './TeacherCardItem';
 import s from './Teacher.module.css';
 
 export const TeacherCard = ({ teachers }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleOpenButton = () => {
-    setIsOpen(!isOpen);
-    return console.log(isOpen);
-  };
   return (
     <div className={s.containerCard}>
       <ul className={s.listCard}>
         {teachers.map(teacher => (
-          <li key={teacher.avatar_url} className={s.listItemCard}>
-            <div className={s.avatarContainer}>
-              <img
-                className={s.avatar}
-                src={teacher.avatar_url}
-                alt={`Avatar of ${teacher.name}`}
-              />
-            </div>
-            <div>
-              <div className={s.containerUpperCard}>
-                <div>
-                  <p className={s.languages}>Languages</p>
-                  <p className={s.name}>
-                    {teacher.name} {teacher.surname}
-                  </p>
-                </div>
-                <div>
-                  <ul className={s.containerUpperInfoCard}>
-                    <li className={s.itemIcon}>
-                      <img className={s.upperIcon} src={book} alt="book"></img>
-                      Lessons online
-                    </li>
-                    <li>Lessons done: {teacher.lessons_done} </li>
-                    <li className={s.itemIcon}>
-                      <img className={s.upperIcon} src={star} alt="star"></img>
-                      Rating: {teacher.rating}
-                    </li>
-                    <li>
-                      Price / 1 hour:{' '}
-                      <span className={s.price}>{teacher.price_per_hour}$</span>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <button className={s.heart}>
-                    <img src={heart} alt="heart" />
-                  </button>
-                </div>
-              </div>
-              <ul className={s.listInfo}>
-                <li>
-                  <span className={s.listInfoSpan}>Speaks:</span>{' '}
-                  <span className={s.listInfoItemLanguage}>
-                    {teacher.languages.join(', ')}
-                  </span>
-                </li>
-                <li>
-                  <span className={s.listInfoSpan}>Lesson Info:</span>{' '}
-                  {teacher.lesson_info}
-                </li>
-                <li>
-                  <span className={s.listInfoSpan}>Conditions:</span>{' '}
-                  {teacher.conditions.join(' ')}
-                </li>
-              </ul>
-              <button onClick={handleOpenButton} className={s.readMore}>
-                Read more
-              </button>
-              {isOpen && (
-                <>
-                  <p className={s.paragraph}>{teacher.experience}</p>
-                  <ul className={s.listReviews}>
-                    {teacher.reviews.map(review => (
-                      <li className={s.listItemReviews}>
-                        <div className={s.containerReviewName}>
-                          <p className={s.reviewName}>{review.reviewer_name}</p>
-                          <p className={s.reviewMark}>
-                            <img src={star} alt="star"></img>
-                            {review.reviewer_rating}
-                          </p>
-                        </div>
-                        <p className={s.reviewText}>{review.comment}</p>
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
-              <ul className={s.btnList}>
-                {teacher.levels.map(level => (
-                  <li>
-                    <button className={s.btnLevel}>{level}</button>
-                  </li>
-                ))}
-              </ul>
-              <button className={s.btnBook}>Book trial lesson</button>
-            </div>
-          </li>
+          <TeacherCardItem key={teacher.avatar_url} teacher={teacher} />
         ))}
       </ul>
     </div>
---
+++ src/pages/Teachers/TeacherCardItem.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import star from '../../images/star.svg';
+import heart from '../../images/heart.svg';
+import book from '../../images/book.svg';
+import s from './Teacher.module.css';
+
+export const TeacherCardItem = ({ teacher }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggleButton = () => {
+    setIsOpen(!isOpen);
+    return console.log(isOpen);
+  };
+  return (
+    <li key={teacher.avatar_url} className={s.listItemCard}>
+      <div className={s.avatarContainer}>
+        <img
+          className={s.avatar}
+          src={teacher.avatar_url}
+          alt={`Avatar of ${teacher.name}`}
+        />
+      </div>
+      <div>
+        <div className={s.containerUpperCard}>
+          <div>
+            <p className={s.languages}>Languages</p>
+            <p className={s.name}>
+              {teacher.name} {teacher.surname}
+            </p>
+          </div>
+          <div>
+            <ul className={s.containerUpperInfoCard}>
+              <li className={s.itemIcon}>
+                <img className={s.upperIcon} src={book} alt="book"></img>
+                Lessons online
+              </li>
+              <li>Lessons done: {teacher.lessons_done} </li>
+              <li className={s.itemIcon}>
+                <img className={s.upperIcon} src={star} alt="star"></img>
+                Rating: {teacher.rating}
+              </li>
+              <li>
+                Price / 1 hour:{' '}
+                <span className={s.price}>{teacher.price_per_hour}$</span>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button className={s.heart}>
+              <img src={heart} alt="heart" />
+            </button>
+          </div>
+        </div>
+        <ul className={s.listInfo}>
+          <li>
+            <span className={s.listInfoSpan}>Speaks:</span>{' '}
+            <span className={s.listInfoItemLanguage}>
+              {teacher.languages.join(', ')}
+            </span>
+          </li>
+          <li>
+            <span className={s.listInfoSpan}>Lesson Info:</span>{' '}
+            {teacher.lesson_info}
+          </li>
+          <li>
+            <span className={s.listInfoSpan}>Conditions:</span>{' '}
+            {teacher.conditions.join(' ')}
+          </li>
+        </ul>
+        {isOpen && (
+          <>
+            <p className={s.paragraph}>{teacher.experience}</p>
+            <ul className={s.listReviews}>
+              {teacher.reviews.map(review => (
+                <li className={s.listItemReviews}>
+                  <div className={s.containerReviewName}>
+                    <p className={s.reviewName}>{review.reviewer_name}</p>
+                    <p className={s.reviewMark}>
+                      <img src={star} alt="star"></img>
+                      {review.reviewer_rating}
+                    </p>
+                  </div>
+                  <p className={s.reviewText}>{review.comment}</p>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        <button onClick={handleToggleButton} className={s.readMore}>
+          {isOpen ? 'Close details' : 'Read more'}
+        </button>
+        <ul className={s.btnList}>
+          {teacher.levels.map(level => (
+            <li>
+              <button className={s.btnLevel}>{level}</button>
+            </li>
+          ))}
+        </ul>
+        <button className={s.btnBook}>Book trial lesson</button>
+      </div>
+    </li>
+  );
+};
