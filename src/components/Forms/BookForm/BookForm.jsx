087ce7076166dd64572c140ac6fed854347a3f1b
Maksym <150538968+Baromental@users.noreply.github.com>
2025-06-23 21:46:35 +0300

    Book Form styles

--- src/components/Forms/BookForm/BookForm.jsx
+++ src/components/Forms/BookForm/BookForm.jsx
@@ -4,54 +4,91 @@ import s from './BookForm.module.css';
 
 export const BookForm = ({ teacher }) => {
   return (
-    <div>
-      <h2>Book trial lesson</h2>
-      <p>
+    <div className={s.bookModal}>
+      <h2 className={s.title}>Book trial lesson</h2>
+      <p className={s.text}>
         Our experienced tutor will assess your current language level, discuss
         your learning goals, and tailor the lesson to your specific needs.
       </p>
-      <div>
+      <div className={s.teacherContainer}>
         <img
           className={s.avatar}
           src={teacher.avatar_url}
           alt={`Avatar of ${teacher.name}`}
         />
-        <div>
-          <p>Your teacher</p>
+        <div className={s.teacherNameContainer}>
+          <p className={s.label}>Your teacher</p>
           <p className={s.name}>
             {teacher.name} {teacher.surname}
           </p>
         </div>
-        <fieldset>
-          <legend>What is your main reason for learning English?</legend>
-          <div>
-            <input type="radio" name="reason" value="career" />
-            <label htmlFor="">Career and business</label>
-          </div>
-          <div>
-            <input type="radio" name="reason" value="kids" />
-            <label htmlFor="">Lesson for kids</label>
-          </div>
-          <div>
-            <input type="radio" name="reason" value="abroad" />
-            <label htmlFor="">Living abroad</label>
-          </div>
-          <div>
-            <input type="radio" name="reason" value="exams" />
-            <label htmlFor="">Exams and coursework</label>
-          </div>
-          <div>
-            <input type="radio" name="reason" value="travel" />
-            <label htmlFor="">Culture, travel or hobby</label>
-          </div>
-        </fieldset>
-        <div>
-          <input type="text" placeholder="Full name" />
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Phone number" />
-        </div>
-        <button>Book</button>
       </div>
+      <fieldset className={s.containerRadio}>
+        <legend className={s.legend}>
+          What is your main reason for learning English?
+        </legend>
+        <div className={s.radioInputContainer}>
+          <input
+            className={s.inputRadio}
+            type="radio"
+            name="reason"
+            value="career"
+          />
+          <label className={s.labelRadio} htmlFor="">
+            Career and business
+          </label>
+        </div>
+        <div className={s.radioInputContainer}>
+          <input
+            className={s.inputRadio}
+            type="radio"
+            name="reason"
+            value="kids"
+          />
+          <label className={s.labelRadio} htmlFor="">
+            Lesson for kids
+          </label>
+        </div>
+        <div className={s.radioInputContainer}>
+          <input
+            className={s.inputRadio}
+            type="radio"
+            name="reason"
+            value="abroad"
+          />
+          <label className={s.labelRadio} htmlFor="">
+            Living abroad
+          </label>
+        </div>
+        <div className={s.radioInputContainer}>
+          <input
+            className={s.inputRadio}
+            type="radio"
+            name="reason"
+            value="exams"
+          />
+          <label className={s.labelRadio} htmlFor="">
+            Exams and coursework
+          </label>
+        </div>
+        <div className={s.radioInputContainer}>
+          <input
+            className={s.inputRadio}
+            type="radio"
+            name="reason"
+            value="travel"
+          />
+          <label className={s.labelRadio} htmlFor="">
+            Culture, travel or hobby
+          </label>
+        </div>
+      </fieldset>
+      <div className={s.containerInputText}>
+        <input className={s.inputText} type="text" placeholder="Full name" />
+        <input className={s.inputText} type="text" placeholder="Email" />
+        <input className={s.inputText} type="text" placeholder="Phone number" />
+      </div>
+      <button className={s.btnBook}>Book</button>
     </div>
   );
 };
