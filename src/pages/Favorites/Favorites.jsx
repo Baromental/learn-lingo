e278ed447dff8939910a7bc006494b5e474c3530
Maksym <150538968+Baromental@users.noreply.github.com>
2025-07-24 00:03:27 +0300

    test fix

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,20 +1,20 @@
-// import { TeacherCard } from 'pages/Teachers/TeacherCard';
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchFavoritesThunk } from '../../redux/favorites/operation';
-// import { selectFavoritesTeachers } from '../../redux/favorites/slice';
+import { TeacherCard } from 'pages/Teachers/TeacherCard';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchFavoritesThunk } from '../../redux/favorites/operation';
+import { selectFavoritesTeachers } from '../../redux/favorites/slice';
 
-// export const Favorites = () => {
-//   const favorites = useSelector(selectFavoritesTeachers);
-//   const dispatch = useDispatch();
+export const Favorites = () => {
+  const favorites = useSelector(selectFavoritesTeachers);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     dispatch(fetchFavoritesThunk());
-//   }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchFavoritesThunk());
+  }, [dispatch]);
 
-//   return (
-//     <div>
-//       <TeacherCard teachers={favorites} />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <TeacherCard teachers={favorites} />
+    </div>
+  );
+};
--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -1,138 +1,138 @@
-// import React, { useEffect, useState } from 'react';
-// import { TeacherCard } from './TeacherCard';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { selectTeachers } from '../../redux/teachers/slice';
-// import { fetchTeachersThunk } from '../../redux/teachers/operations';
-// import s from './Teacher.module.css';
+import React, { useEffect, useState } from 'react';
+import { TeacherCard } from './TeacherCard';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectTeachers } from '../../redux/teachers/slice';
+import { fetchTeachersThunk } from '../../redux/teachers/operations';
+import s from './Teacher.module.css';
 
-// export const Teachers = () => {
-//   const [visibleTeachers, setVisibleTeachers] = useState([]);
-//   const [displayCount, setDisplayCount] = useState(4);
-//   const [filters, setFilters] = useState({
-//     language: '',
-//     level: '',
-//     price: '',
-//   });
-//   const teachers = useSelector(selectTeachers);
-//   const dispatch = useDispatch();
+export const Teachers = () => {
+  const [visibleTeachers, setVisibleTeachers] = useState([]);
+  const [displayCount, setDisplayCount] = useState(4);
+  const [filters, setFilters] = useState({
+    language: '',
+    level: '',
+    price: '',
+  });
+  const teachers = useSelector(selectTeachers);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     dispatch(fetchTeachersThunk());
-//   }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchTeachersThunk());
+  }, [dispatch]);
 
-//   useEffect(() => {
-//     const filteredTeachers = handleFilter(teachers, filters);
-//     setVisibleTeachers(filteredTeachers.slice(0, displayCount));
-//   }, [teachers, filters, displayCount]);
+  useEffect(() => {
+    const filteredTeachers = handleFilter(teachers, filters);
+    setVisibleTeachers(filteredTeachers.slice(0, displayCount));
+  }, [teachers, filters, displayCount]);
 
-//   const handleFilter = (teachers, filters) => {
-//     return teachers.filter(teachers => {
-//       const matchLang = filters.language
-//         ? teachers.languages.includes(filters.language)
-//         : true;
-//       const matchLevel = filters.level
-//         ? teachers.levels.includes(filters.level)
-//         : true;
-//       const matchPrice = filters.price
-//         ? teachers.price_per_hour <= Number(filters.price)
-//         : true;
-//       return matchLang && matchLevel && matchPrice;
-//     });
-//   };
+  const handleFilter = (teachers, filters) => {
+    return teachers.filter(teachers => {
+      const matchLang = filters.language
+        ? teachers.languages.includes(filters.language)
+        : true;
+      const matchLevel = filters.level
+        ? teachers.levels.includes(filters.level)
+        : true;
+      const matchPrice = filters.price
+        ? teachers.price_per_hour <= Number(filters.price)
+        : true;
+      return matchLang && matchLevel && matchPrice;
+    });
+  };
 
-//   const handleResetFilters = () => {
-//     setFilters({ language: '', level: '', price: '' });
-//     setDisplayCount(4);
-//   };
+  const handleResetFilters = () => {
+    setFilters({ language: '', level: '', price: '' });
+    setDisplayCount(4);
+  };
 
-//   const handleLoadMore = () => {
-//     setDisplayCount(prev => prev + 4);
-//   };
+  const handleLoadMore = () => {
+    setDisplayCount(prev => prev + 4);
+  };
 
-//   return (
-//     <div className={s.container}>
-//       <div className={s.selectContainer}>
-//         <ul className={s.listSelect}>
-//           <li className={s.listItemSelect}>
-//             <label className={s.labelSelect} htmlFor="languages">
-//               Languages
-//             </label>
-//             <select
-//               value={filters.language}
-//               onChange={e =>
-//                 setFilters(prev => ({ ...prev, language: e.target.value }))
-//               }
-//               className={s.select}
-//               name="languages"
-//               id="languages"
-//             >
-//               <option value="">All</option>
-//               <option value="English">English</option>
-//               <option value="French">French</option>
-//               <option value="German">German</option>
-//               <option value="Italian">Italian</option>
-//               <option value="Spanish">Spanish</option>
-//               <option value="Korean">Korean</option>
-//               <option value="Vietnamese">Vietnamese</option>
+  return (
+    <div className={s.container}>
+      <div className={s.selectContainer}>
+        <ul className={s.listSelect}>
+          <li className={s.listItemSelect}>
+            <label className={s.labelSelect} htmlFor="languages">
+              Languages
+            </label>
+            <select
+              value={filters.language}
+              onChange={e =>
+                setFilters(prev => ({ ...prev, language: e.target.value }))
+              }
+              className={s.select}
+              name="languages"
+              id="languages"
+            >
+              <option value="">All</option>
+              <option value="English">English</option>
+              <option value="French">French</option>
+              <option value="German">German</option>
+              <option value="Italian">Italian</option>
+              <option value="Spanish">Spanish</option>
+              <option value="Korean">Korean</option>
+              <option value="Vietnamese">Vietnamese</option>
 
-//               <option value="Mandarin Chinese">Mandarin Chinese</option>
-//             </select>
-//           </li>
-//           <li className={s.listItemSelect}>
-//             <label className={s.labelSelect} htmlFor="level">
-//               Level of knowledge
-//             </label>
-//             <select
-//               value={filters.level}
-//               onChange={e =>
-//                 setFilters(prev => ({ ...prev, level: e.target.value }))
-//               }
-//               className={s.select}
-//               name="level"
-//               id="level"
-//             >
-//               <option value="">All</option>
-//               <option value="A1 Beginner">A1 Beginner</option>
-//               <option value="A2 Elementary">A2 Elementary</option>
-//               <option value="B1 Intermediate">B1 Intermediate</option>
-//               <option value="B2 Upper-Intermediate">
-//                 B2 Upper-Intermediate
-//               </option>
-//               <option value="C1 Advanced">C1 Advanced</option>
-//               <option value="C2 Proficient">C2 Proficient</option>
-//             </select>
-//           </li>
-//           <li className={s.listItemSelect}>
-//             <label className={s.labelSelect} htmlFor="price">
-//               Price
-//             </label>
-//             <select
-//               value={filters.price}
-//               onChange={e =>
-//                 setFilters(prev => ({ ...prev, price: e.target.value }))
-//               }
-//               className={s.select}
-//               name="price"
-//               id="price"
-//             >
-//               <option value="">All</option>
-//               <option value="25">25 $</option>
-//               <option value="30">30 $</option>
-//               <option value="35">35 $</option>
-//               <option value="40">40 $</option>
-//             </select>
-//           </li>
-//         </ul>
-//         <button onClick={handleResetFilters} className={s.btnReset}>
-//           Reset filters
-//         </button>
-//       </div>
-//       <TeacherCard teachers={visibleTeachers} />
-//       {visibleTeachers.length < handleFilter(teachers, filters).length && (
-//         <button onClick={handleLoadMore} className={s.btnLoad}>
-//           Load more
-//         </button>
-//       )}
-//     </div>
-//   );
-// };
+              <option value="Mandarin Chinese">Mandarin Chinese</option>
+            </select>
+          </li>
+          <li className={s.listItemSelect}>
+            <label className={s.labelSelect} htmlFor="level">
+              Level of knowledge
+            </label>
+            <select
+              value={filters.level}
+              onChange={e =>
+                setFilters(prev => ({ ...prev, level: e.target.value }))
+              }
+              className={s.select}
+              name="level"
+              id="level"
+            >
+              <option value="">All</option>
+              <option value="A1 Beginner">A1 Beginner</option>
+              <option value="A2 Elementary">A2 Elementary</option>
+              <option value="B1 Intermediate">B1 Intermediate</option>
+              <option value="B2 Upper-Intermediate">
+                B2 Upper-Intermediate
+              </option>
+              <option value="C1 Advanced">C1 Advanced</option>
+              <option value="C2 Proficient">C2 Proficient</option>
+            </select>
+          </li>
+          <li className={s.listItemSelect}>
+            <label className={s.labelSelect} htmlFor="price">
+              Price
+            </label>
+            <select
+              value={filters.price}
+              onChange={e =>
+                setFilters(prev => ({ ...prev, price: e.target.value }))
+              }
+              className={s.select}
+              name="price"
+              id="price"
+            >
+              <option value="">All</option>
+              <option value="25">25 $</option>
+              <option value="30">30 $</option>
+              <option value="35">35 $</option>
+              <option value="40">40 $</option>
+            </select>
+          </li>
+        </ul>
+        <button onClick={handleResetFilters} className={s.btnReset}>
+          Reset filters
+        </button>
+      </div>
+      <TeacherCard teachers={visibleTeachers} />
+      {visibleTeachers.length < handleFilter(teachers, filters).length && (
+        <button onClick={handleLoadMore} className={s.btnLoad}>
+          Load more
+        </button>
+      )}
+    </div>
+  );
+};
